0c1818323149427837986d08666fa51bec384f1f
Dallas <dkea201@mywhitecliffe.com>
2024-06-20 17:30:32 +1200

    Configured settings and views
    > Create root templates folder
    > Configured CSS file and pathing

--- schoolApp/school/views.py
+++ schoolApp/school/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import Article
 
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Hello Team")
+class Home(ListView):
+    template_name = 'school/home.html'
+    model = Article
+    context_object_name = 'students'
