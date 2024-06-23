5b1ad0987cd713a940356b4dae3be74e06091203
AManga <anuradha012@gmail.com>
2024-06-23 21:22:36 +1200

    Implemented login, signup and logout.

--- schoolApp/schoolApp/urls.py
+++ schoolApp/schoolApp/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('school.urls', namespace='school')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ]
-
-
