4f698530b62a0b01fd06e10db737b607d83eab75
rajichebrolu <raji.chebrolu@gmail.com>
2024-06-24 19:01:52 +1200

    Database connected to MySQL

--- schoolApp/schoolApp/settings.py
+++ schoolApp/schoolApp/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'schoolApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        'NAME': "schoolappdatabase",
+        "USER": "root",
+        "PASSWORD": "Charithapala@24",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
+
 
 
 # Password validation
