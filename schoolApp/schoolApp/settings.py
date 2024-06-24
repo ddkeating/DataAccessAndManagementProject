2f5b3306ace4834fe3310b2b2d315c07971968e4
rajichebrolu <raji.chebrolu@gmail.com>
2024-06-24 20:04:54 +1200

    MySQL Database connected

--- schoolApp/schoolApp/settings.py
+++ schoolApp/schoolApp/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         'NAME': "schoolappdatabase",
         "USER": "root",
-        "PASSWORD": "Charithapala@24",
+        "PASSWORD": "<<<Place Your Password>>>",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
