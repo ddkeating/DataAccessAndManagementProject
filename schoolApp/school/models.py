b41f5d48ee8311994f3f28bc215d1b91c4994c3f
SacredRider <73513022+SacredRider02@users.noreply.github.com>
2024-06-26 14:06:13 +1200

    data insertions fixed

--- schoolApp/school/models.py
+++ schoolApp/school/models.py
@@ -3,26 +3,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length=250, unique= True) 
-
 class Article(models.Model):
-    
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    article_id = models.AutoField(primary_key=True, editable=False)
+    category = models.CharField(max_length=255)
     type = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     born = models.CharField(max_length=15, null=True, blank=True)
     died = models.CharField(max_length=15, null=True, blank=True)
     nationality = models.CharField(max_length=50, null=True, blank=True)
-    known_for = models.CharField(max_length=100, null=True, blank=True)
-    notable_work = models.TextField(null=True, blank=True)
-    about = models.CharField(max_length=6000)
+    known_for = models.CharField(max_length=255, null=True, blank=True)
+    notable_work = models.CharField(max_length=255, null=True, blank=True)
+    about = models.TextField(max_length=60000)
     year = models.CharField(max_length=15, null=True, blank=True)
-    medium = models.CharField(max_length=50, null=True, blank=True)
-    dimensions = models.CharField(max_length=50, null=True, blank=True)
-    location = models.CharField(max_length=50, null=True, blank=True)
-    designed_by = models.CharField(max_length=50, null=True, blank=True)
-    developer = models.CharField(max_length=50, null=True, blank=True)
+    medium = models.CharField(max_length=250, null=True, blank=True)
+    dimensions = models.CharField(max_length=250, null=True, blank=True)
+    location = models.CharField(max_length=250, null=True, blank=True)
+    designed_by = models.CharField(max_length=250, null=True, blank=True)
+    developer = models.CharField(max_length=250, null=True, blank=True)
 
     
 
