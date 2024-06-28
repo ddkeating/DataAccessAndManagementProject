0f7cba5ce926e56b07a04b8737525a1492233567
Dallas <dkea201@mywhitecliffe.com>
2024-06-28 13:28:56 +1200

    Tidying Up

--- schoolApp/schoolApp/settings.py
+++ schoolApp/schoolApp/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         'NAME': "schoolappdatabase",
         "USER": "root",
-        "PASSWORD": "ilLsm541",
+        "PASSWORD": "<<<your password>>>",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
